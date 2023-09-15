c0630f1404d68d5a7df36ec35195c9b2234faa5d
lyran10 <liranramekar629@gmail.com>
2023-09-15 23:31:52 +0530

    portfolio

--- src/components/symbol/symbol.tsx
+++ src/components/symbol/symbol.tsx
@@ -33,11 +33,11 @@ export const Symbol = () => {
   return (
     <div className='flex justify-center items-center w-full lg:w-[100%]'>
     <div className='m-auto text-[10px] flex justify-center h-[100px] items-center'>
-         <div className='positive  relative mr-5 text-[#f5f5f5] text-[17px] md:text-[30px] lg:text-[30px] duration-500 py-1 px-2 before:absolute before:content-"" before:h-[1px] before:w-[50%] before:bg-[#526D82] before:right-[110%] before:top-[55%] '>Ever Positive</div>
+         <div className='positive  relative mr-5 text-[#f5f5f5] text-[17px] md:text-[30px] lg:text-[30px] duration-500 py-1 px-2 before:absolute before:hidden before:content-"" before:h-[1px] before:w-[50%] before:bg-[#526D82] before:right-[110%] before:top-[55%] '>Ever Positive</div>
          <div className='symbol w-[100px] h-[100px]'>
           <img className='w-full h-full rounded-full' src={symbol} alt="" />
          </div>
-        <div className='negative relative ml-5 text-[#f5f5f5] duration-500 py-1 px-2 before:absolute before:content-"" before:h-[1px] before:w-[50%] before:bg-[#526D82] before:left-[110%] before:top-[55%] text-[17px] md:text-[30px] lg:text-[30px]'>Never Negative</div>
+        <div className='negative relative ml-5 text-[#f5f5f5] duration-500 py-1 px-2 before:absolute before:content-"" before:h-[1px] before:w-[50%] before:bg-[#526D82] before:hidden before:left-[110%] before:top-[55%] text-[17px] md:text-[30px] lg:text-[30px]'>Never Negative</div>
     </div>
     </div>
   )
